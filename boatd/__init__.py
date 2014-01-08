d0957a96535174a640aad9153f47659cdba2a04e
Louis Taylor <kragniz@gmail.com>
2014-01-08 21:28:38 +0000

    Basic driver and behaviour loading

--- boatd/__init__.py
+++ boatd/__init__.py
@@ -1,5 +1,32 @@
-from functools import wraps
+from __future__ import print_function
+
+import imp
+import sys
+import os
 import time
+from functools import wraps
+
+class Boat(object):
+    def __init__(self):
+        pass
+
+    def do_hardware(self, n):
+        print(n)
+
+    def heading(self):
+        return 3.4
+
+def module_name(path):
+    return os.path.splitext(os.path.split(path)[-1])[0]
+
+def inject_import(filename, inject):
+    name = module_name(filename)
+    module = imp.new_module(name)
+    vars(module).update(inject)
+    with open(filename) as f:
+        exec f.read() in vars(module)
+    sys.modules[name] = module
+    return module
 
 def log(message):
     print(time.strftime('[%H:%M:%S]'), message)
@@ -11,3 +38,12 @@ def do_something(func):
         log('did something and got {}'.format(r))
         return r
     return inner
+
+assert len(sys.argv) > 2
+boatd = imp.new_module('boatd')
+vars(boatd).update(globals())
+drive_path = sys.argv[1]
+inject_import(drive_path, {'boatd': boatd})
+
+behaviour_path = sys.argv[2]
+inject_import(behaviour_path, {'boat': Boat()})
