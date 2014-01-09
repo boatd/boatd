74dff495d7c0a953ef004d41a5aa45b7770c3407
Louis Taylor <kragniz@gmail.com>
2014-01-09 19:04:28 +0000

    Add docstring to config class

--- boatd/config.py
+++ boatd/config.py
@@ -1,6 +1,28 @@
 import json
 
 class Config(object):
+    '''
+    Allow for more natural access to the member values of a dictionary. For
+    example, instead of writing:
+
+        d = {
+              'a': {
+                'x': 5,
+                'y': 10
+              },
+              'b': {
+                'z': 42
+              }
+             }
+
+        d['a']['x']
+
+    we can instead write:
+
+        c = Config(d)
+        c.a.x
+    '''
+
     def __init__(self, d):
         self.__dict__.update(d)
         for k, i in self.__dict__.iteritems():
