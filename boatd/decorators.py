1529c31cba83fe9370d25c041193197cd463b7de
Louis Taylor <kragniz@gmail.com>
2014-01-10 19:04:48 +0000

    Fix indentation issues in decorators.py

--- boatd/decorators.py
+++ boatd/decorators.py
@@ -4,12 +4,13 @@ from . import logging
 
 def build_decorator(called_func):
     def dec(func):
-         @wraps(func)
-         def inner(*args, **kwargs):
-             return_value = func(*args, **kwargs)
-             called_func(return_value)
-             return return_value
-         return inner
+        @wraps(func)
+        def inner(*args, **kwargs):
+            return_value = func(*args, **kwargs)
+            called_func(return_value)
+            return return_value
+        return inner
     return dec
 
 do_something = build_decorator(lambda x: logging.log('did something and got {}'.format(x)))
+heading = build_decorator(lambda x: logging.log('heading: {}'.format(x)))
