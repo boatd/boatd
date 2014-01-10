9dd8c4bfce4dabcea036f2f7f74a77b85c041311
Louis Taylor <kragniz@gmail.com>
2014-01-10 20:26:31 +0000

    Add before and after functions to the decorator

--- boatd/decorators.py
+++ boatd/decorators.py
@@ -2,15 +2,29 @@ from functools import wraps
 
 from . import logging
 
-def build_decorator(called_func):
+def maybe_run(func, *args, **kwargs):
+    if func is not None:
+        return func(*args, **kwargs)
+    else:
+        return None
+
+def build_decorator(before_func=None,
+                    after_func=None):
     def dec(func):
         @wraps(func)
         def inner(*args, **kwargs):
+            maybe_run(before_func)
             return_value = func(*args, **kwargs)
-            called_func(return_value)
+            maybe_run(after_func, return_value)
             return return_value
         return inner
     return dec
 
-do_something = build_decorator(lambda x: logging.log('did something and got {}'.format(x)))
-heading = build_decorator(lambda x: logging.log('heading: {}'.format(x)))
+do_something = build_decorator(
+                   after_func=lambda x: logging.log('did something and got {}'.format(x))
+               )
+
+heading = build_decorator(
+            lambda: logging.log('requested heading'),
+            lambda x: logging.log('got heading: {}'.format(x))
+          )
