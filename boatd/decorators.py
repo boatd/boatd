038a338ea8e187ccc52c0b2b99fb8553f09072ba
Louis Taylor <kragniz@gmail.com>
2014-01-10 03:59:30 +0000

    Add decorator builder

--- boatd/decorators.py
+++ boatd/decorators.py
@@ -2,10 +2,14 @@ from functools import wraps
 
 from . import logging
 
-def do_something(func):
-     @wraps(func)
-     def inner(*args, **kwargs):
-         return_value = func(*args, **kwargs)
-         logging.log('did something and got {}'.format(return_value))
-         return return_value
-     return inner
+def build_decorator(called_func):
+    def dec(func):
+         @wraps(func)
+         def inner(*args, **kwargs):
+             return_value = func(*args, **kwargs)
+             called_func(return_value)
+             return return_value
+         return inner
+    return dec
+
+do_something = build_decorator(lambda x: logging.log('did something and got {}'.format(x)))
