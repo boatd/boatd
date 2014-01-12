a72f19d4913b9c3a87cd2cf3560e2411c185ea59
Louis Taylor <kragniz@gmail.com>
2014-01-12 00:41:09 +0000

    Add docstrings to Boat

--- boatd/boat.py
+++ boatd/boat.py
@@ -1,8 +1,10 @@
 class Boat(object):
+    '''The boat itself. Most of the work is done by the active driver'''
     def __init__(self, driver):
         self.driver = driver
 
     def __getattr__(self, name):
+        '''Return the requested attribute from the currently loaded driver'''
         func = vars(self.driver.module).get(name)
         if func is None:
             raise AttributeError(
