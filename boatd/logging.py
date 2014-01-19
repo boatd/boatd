dc908bbf7713f608fcbe3cb0a5454d8ba6895368
Louis Taylor <kragniz@gmail.com>
2014-01-19 18:49:47 +0000

    Add color function to logging

--- boatd/logging.py
+++ boatd/logging.py
@@ -5,5 +5,8 @@ import time
 VERBOSE, NORMAL, WARN, ERROR = range(4)
 
 
+def color(message, text_color, text_style=0):
+    return '\033[{};{}m{}\033[0m'.format(text_style, text_color, message)
+
 def log(message, level=NORMAL):
     print(time.strftime('[%H:%M:%S]'), message)
