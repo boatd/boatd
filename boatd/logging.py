35809afd213875009abc713a19dbcee01967c8c7
Louis Taylor <kragniz@gmail.com>
2014-01-19 18:50:24 +0000

    Add a colored warn message with warn level logging

--- boatd/logging.py
+++ boatd/logging.py
@@ -9,4 +9,9 @@ def color(message, text_color, text_style=0):
     return '\033[{};{}m{}\033[0m'.format(text_style, text_color, message)
 
 def log(message, level=NORMAL):
-    print(time.strftime('[%H:%M:%S]'), message)
+    messages = []
+    if level == WARN:
+        messages.append('[{}]'.format(color('WARNING', 31)))
+
+    messages.append(message)
+    print(time.strftime('[%H:%M:%S]'), *messages)
