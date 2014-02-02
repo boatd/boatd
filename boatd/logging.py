a80e318be22a29ba32392bb0b8ce38a4e87d3abe
Louis Taylor <kragniz@gmail.com>
2014-02-02 02:46:34 +0000

    Add docstring to logging.color

--- boatd/logging.py
+++ boatd/logging.py
@@ -6,6 +6,7 @@ VERBOSE, NORMAL, WARN, ERROR = range(4)
 
 
 def color(message, text_color, text_style=0):
+    '''Return the message wrapped in ansi color code'''
     return '\033[{};{}m{}\033[0m'.format(text_style, text_color, message)
 
 
