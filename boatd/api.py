74f55349dfecf0cc5d272f9eb37c9429ea79f349
Louis Taylor <kragniz@gmail.com>
2014-02-24 17:51:23 +0000

    Add start of http server

--- boatd/api.py
+++ boatd/api.py
@@ -1,22 +1,10 @@
-from twisted.web.server import Site
-from twisted.web.resource import Resource
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
-class Root(Resource):
-    def render_GET(self, request):
-        return 'sup, boat'
-
-class Boatd(Resource):
-    def getChild(self, name, request):
-        uri = request.uri
-        print uri
-        return Root()
-
-root = Boatd()
-factory = Site(root)
+class BoatdRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self, *args, **kwargs):
+        print('Requested', self.path)
 
 if __name__ == '__main__':
-    from twisted.internet import reactor
-    print 'starting on port 6969'
-
-    reactor.listenTCP(6969, factory)
-    reactor.run()
+    httpd = HTTPServer(('', 8000),
+            BoatdRequestHandler)
+    httpd.serve_forever()
