8bd2a8d6c81f4e9dc549e3d8cffb39aae12dcbd0
Louis Taylor <kragniz@gmail.com>
2014-02-25 13:47:31 +0000

    Add support for python3 in api.py

--- boatd/api.py
+++ boatd/api.py
@@ -1,4 +1,8 @@
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+try:
+    from http.server import HTTPServer, BaseHTTPRequestHandler
+except ImportError:
+    from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
 
 class BoatdRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self, *args, **kwargs):
@@ -6,7 +10,7 @@ class BoatdRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-Type', 'application/JSON')
         self.end_headers()
-        self.request.sendall('hi there')
+        self.request.sendall('hi there'.encode())
 
 if __name__ == '__main__':
     httpd = HTTPServer(('', 8000),
