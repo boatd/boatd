d01541bbfb0bea0e28bdafe998ab2ce16168e818
Louis Taylor <kragniz@gmail.com>
2014-02-25 13:32:06 +0000

    Add correct headers

--- boatd/api.py
+++ boatd/api.py
@@ -3,6 +3,10 @@ from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 class BoatdRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self, *args, **kwargs):
         print('Requested', self.path)
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/JSON')
+        self.end_headers()
+        self.request.sendall('hi there')
 
 if __name__ == '__main__':
     httpd = HTTPServer(('', 8000),
