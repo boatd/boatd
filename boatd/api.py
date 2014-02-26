6310e91d0f0037053fe5e003ea30bdc132c3d23c
Louis Taylor <kragniz@gmail.com>
2014-02-26 15:10:16 +0000

    Change port to 2222

--- boatd/api.py
+++ boatd/api.py
@@ -5,6 +5,10 @@ except ImportError:
 
 
 class BoatdRequestHandler(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        print(args, kwargs)
+        super(BoatdRequestHandler, self).__init__(*args, **kwargs)
+
     def do_GET(self, *args, **kwargs):
         print('Requested', self.path)
         self.send_response(200)
@@ -13,6 +17,6 @@ class BoatdRequestHandler(BaseHTTPRequestHandler):
         self.request.sendall('hi there'.encode())
 
 if __name__ == '__main__':
-    httpd = HTTPServer(('', 8000),
-            BoatdRequestHandler)
+    httpd = HTTPServer(('', 2222),
+        BoatdRequestHandler)
     httpd.serve_forever()
