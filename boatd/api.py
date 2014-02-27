2fcb9c28e07f0d955810f0fbafb5f6ffaf7bd710
Louis Taylor <kragniz@gmail.com>
2014-02-27 14:34:20 +0000

    Add BoatdHTTPServer
    Subclasses HTTPServer and adds a boat attribute

--- boatd/api.py
+++ boatd/api.py
@@ -3,19 +3,29 @@ try:
 except ImportError:
     from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
+import os
+import logging
+
+class BoatdHTTPServer(HTTPServer):
+    def __init__(self, boat,
+            server_address, RequestHandlerClass, bind_and_activate=True):
+
+        HTTPServer.__init__(self, server_address, RequestHandlerClass, bind_and_activate)
+        self.boat = boat
 
 class BoatdRequestHandler(BaseHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
+    server_version = 'boatd/0.1'
 
     def do_GET(self, *args, **kwargs):
-        print('Requested', self.path)
         self.send_response(200)
         self.send_header('Content-Type', 'application/JSON')
         self.end_headers()
-        self.request.sendall('hi there'.encode())
+        self.request.sendall('hi there {}\n'.format(self.server.boat).encode())
+
+    def log_request(self, code='-', size='-'):
+        logging.log('REST request {}'.format(self.path), level=logging.VERBOSE)
 
 if __name__ == '__main__':
-    httpd = HTTPServer(('', 2222),
+    httpd = BoatdHTTPServer(object(), ('', 2222),
         BoatdRequestHandler)
     httpd.serve_forever()
