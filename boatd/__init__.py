39ef18346626cec427aec61aa35ee7e3abf00905
Louis Taylor <kragniz@gmail.com>
2014-03-06 07:25:10 +0000

    Remove old behavior module loading

--- boatd/__init__.py
+++ boatd/__init__.py
@@ -11,25 +11,6 @@ from .driver import Driver
 from .api import BoatdHTTPServer, BoatdRequestHandler
 
 
-def inject_import(name, filename, inject):
-    module = imp.new_module(name)
-    vars(module).update(inject)
-    with open(filename) as f:
-        exec(f.read(), vars(module))
-    return module
-
-
-class Behaviour(object):
-    def __init__(self, behaviour_path, boat):
-        self.boat = boat
-        self.path = behaviour_path
-
-    def run(self):
-        return inject_import('behaviour',
-                             self.path,
-                             {'boat': self.boat})
-
-
 def main():
     if len(sys.argv) > 1:
         conf_file = sys.argv[1]
