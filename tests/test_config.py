d3809386b46b725747fde048a34e3db684870e1d
Louis Taylor <kragniz@gmail.com>
2014-03-10 17:53:51 +0000

    Add setup method to config test class

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,8 +2,10 @@ import boatd
 import os
 
 class TestConfig(object):
-    def test_yaml(self):
-        print(__file__)
-        self.config = boatd.Config.from_yaml('config.yaml')
-        assert self.config.boatd.port == 2222
-        
+    def setup(self):
+        self.directory, _ = os.path.split(__file__)
+        self.yaml_file = os.path.join(self.directory, 'config.yaml')
+
+    def test_load_yaml(self):
+        config = boatd.Config.from_yaml(self.yaml_file)
+        assert config.boatd
