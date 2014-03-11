b403b99653088fe7fb672723cd31ab008a7a99f1
Louis Taylor <kragniz@gmail.com>
2014-03-11 00:21:18 +0000

    Test driver loading

--- boatd/tests/test_driver.py
+++ boatd/tests/test_driver.py
@@ -6,14 +6,19 @@ import boatd
 class TestDriver(object):
     def setup(self):
         self.directory, _ = os.path.split(__file__)
-        self.driver_file = os.path.join(self.directory, 'driver.py')
 
         configuration = {
             'scripts': {
-                'driver': 'driver.py'
+                'driver': os.path.join(self.directory, 'driver.py')
             }
         }
         self.mock_config = boatd.Config(configuration)
 
-    def test_config(self):
-        assert self.mock_config.scripts
+        self.driver_file = os.path.join(self.directory,
+            self.mock_config.scripts.driver)
+
+    def test_driver_file(self):
+        assert os.path.isfile(self.driver_file)
+
+    def test_loading_driver(self):
+        assert boatd.load_driver(self.mock_config)
