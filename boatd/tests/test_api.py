f8d2114a8392018e96c66749120b2308b0e9c949
Louis Taylor <kragniz@gmail.com>
2014-03-12 14:31:20 +0000

    Catch socket.error
    Also pep8 formatting

--- boatd/tests/test_api.py
+++ boatd/tests/test_api.py
@@ -4,15 +4,19 @@ except ImportError:
     from urllib2 import urlopen
 
 import threading
+import socket
 
 import boatd
+
 
 class MockBoat(object):
     def __init__(self):
         self.heading = 45
 
+
 class TestAPI(object):
     TEST_PORTS = 10
+
     def __init__(self):
         self.port = 2222
 
@@ -20,9 +24,9 @@ class TestAPI(object):
         for _ in range(self.TEST_PORTS):
             try:
                 httpd = boatd.BoatdHTTPServer(MockBoat(), ('', self.port),
-                            boatd.BoatdRequestHandler)
+                                              boatd.BoatdRequestHandler)
                 break
-            except Exception, e:
+            except socket.error:
                 self.port += 1
 
         self.http_thread = threading.Thread(target=httpd.handle_request)
