c3c70a3e8f367ad6b2cf09318b4a2b0162f9f3db
Louis Taylor <kragniz@gmail.com>
2014-03-13 14:36:01 +0000

    Abstract the url to the local instance of boatd

--- boatd/tests/test_api.py
+++ boatd/tests/test_api.py
@@ -35,27 +35,33 @@ class TestAPI(object):
         self.http_thread.daemon = True
         self.http_thread.start()
 
+    def _base_url(self):
+        return 'http://localhost:{}'.format(self.port)
+
+    def _url(self, endpoint):
+        return self._base_url() + endpoint
+
     def test_thread(self):
         assert self.http_thread.is_alive()
 
     def test_GET(self):
-        assert urlopen('http://localhost:{}'.format(self.port)).read()
+        assert urlopen(self._base_url()).read()
 
     def test_valid_json(self):
-        content = urlopen('http://localhost:{}'.format(self.port)).read()
+        content = urlopen(self._base_url()).read()
         d = json.loads(content.decode("utf-8"))
         assert 'boatd' in d
 
     def test_request_pony(self):
-        content = urlopen('http://localhost:{}/pony'.format(self.port)).read()
+        content = urlopen(self._url('/pony')).read()
         d = json.loads(content.decode("utf-8"))
         assert d.get('result') == 'magic'
 
     def test_request_heading(self):
-        content = urlopen('http://localhost:{}/heading'.format(self.port)).read()
+        content = urlopen(self._url('/heading')).read()
         d = json.loads(content.decode("utf-8"))
         assert d.get('heading') == 45
 
     def test_content_type(self):
-        m = urlopen('http://localhost:{}/heading'.format(self.port)).info()
+        m = urlopen(self._url('/heading')).info()
         assert m['content-type'] == 'application/JSON'
