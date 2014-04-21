3cf31838e24a6f558069e0883c6cfd3c887266c6
Louis Taylor <kragniz@gmail.com>
2014-04-21 03:58:19 +0000

    Clean log directory in tests

--- boatd/tests/test_logging.py
+++ boatd/tests/test_logging.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 import boatd
 
 class MockBoat(object):
@@ -8,9 +11,18 @@ class MockBoat(object):
         self.pos += 1
         return (self.pos, self.pos)
 
-class TestLogging(object):
+class TestGpxLogging(object):
     def setup(self):
         self.boat = MockBoat()
+        self.conf = boatd.Config.from_yaml('boatd-config.yaml')
+
+        log_dir = os.path.dirname(self.conf.boatd.log.gpx.filename)
+        if os.path.isdir(log_dir):
+            #make sure the log directory is clean
+            shutil.rmtree(log_dir)
 
     def test_mock(self):
         assert self.boat.position() == (1, 1)
+
+    def test_correct_config_file(self):
+        assert self.conf.name == 'boatd-test-config'
