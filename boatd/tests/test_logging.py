edb96d3d468476d46cea1223e818b9d152e12378
Louis Taylor <kragniz@gmail.com>
2014-04-22 09:34:23 +0100

    Add some simple tests for a clean log environment

--- boatd/tests/test_logging.py
+++ boatd/tests/test_logging.py
@@ -16,13 +16,19 @@ class TestGpxLogging(object):
         self.boat = MockBoat()
         self.conf = boatd.Config.from_yaml('boatd-config.yaml')
 
-        log_dir = os.path.dirname(self.conf.boatd.log.gpx.filename)
-        if os.path.isdir(log_dir):
+        base_name = self.conf.boatd.log.gpx.filename
+        self.log_dir, name = os.path.split(base_name)
+        if os.path.exists(self.log_dir):
             #make sure the log directory is clean
-            shutil.rmtree(log_dir)
+            shutil.rmtree(self.log_dir)
+
+        self.logger = boatd.logging.GpxLogger(self.boat, base_name)
 
     def test_mock(self):
         assert self.boat.position() == (1, 1)
 
     def test_correct_config_file(self):
         assert self.conf.name == 'boatd-test-config'
+
+    def test_dir_created(self):
+        assert os.path.exists(self.log_dir)
