06fbc3ce6cfef66bc42aa420478972a1dd8800be
Louis Taylor <kragniz@gmail.com>
2014-06-05 21:34:18 +0100

    Add degrees_to_nmea function

--- boatd/nmea.py
+++ boatd/nmea.py
@@ -1,3 +1,5 @@
+import math
+
 def calculate_checksum(line):
     '''Return the NMEA checksum for a given line'''
     x = 0
@@ -9,6 +11,11 @@ def calculate_checksum(line):
 def nmea_line(line):
     checksum = calculate_checksum(line)
     return '$' + line + '*' + checksum
+
+def degrees_to_nmea(input_degrees):
+    degrees = math.trunc(input_degrees)
+    minutes = (input_degrees - degrees) * 60
+    return '{}{:.3g}'.format(degrees, minutes)
 
 def hdm(heading):
     '''Return a HDM nmea sentance from a given heading'''
