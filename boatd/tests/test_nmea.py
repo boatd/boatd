c3119e0560e88e2f2c60ab4de499952c0aebb025
Louis Taylor <kragniz@gmail.com>
2014-06-08 00:37:00 -0700

    Remove incorrect leading zero

--- boatd/tests/test_nmea.py
+++ boatd/tests/test_nmea.py
@@ -12,6 +12,6 @@ def test_negative_degrees_to_nmea():
     assert boatd.nmea.degrees_to_nmea(-45.555) == '-4533.3'
 
 def test_gll():
-    date = datetime.datetime(2014, 6, 7, 22, 22, 01, 152642)
+    date = datetime.datetime(2014, 6, 7, 22, 22, 1, 152642)
     assert boatd.nmea.gll(38.063413, -122.240910, date) == \
         '$GLL,383.8,N,12214.5,W,222201.15,A*35'
