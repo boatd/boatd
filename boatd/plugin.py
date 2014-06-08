c9adf9c48297c7fd2e7820fb4008263e54a74f5b
Louis Taylor <kragniz@gmail.com>
2014-06-08 10:13:44 -0700

    Complete the finding of modules

--- boatd/plugin.py
+++ boatd/plugin.py
@@ -4,5 +4,7 @@ def find_plugins(search_directories):
     found_plugins = []
     for directory in search_directories:
         for plugin in os.listdir(directory):
-            if directory.endswith('.py'):
-                found_plugins.append(os.path.join(directory, plugin)
+            if plugin.endswith('.py'):
+                found_plugins.append(os.path.join(directory, plugin))
+
+    return found_plugins
