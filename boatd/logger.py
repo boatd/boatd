39c98624382bc9cb0cf62447782ee1e3237c7706
Louis Taylor <louis@kragniz.eu>
2015-09-06 04:01:34 +0100

    Remove old logging handler

--- boatd/logger.py
+++ boatd/logger.py
@@ -1,24 +1,4 @@
-from __future__ import print_function
-
 import logging
-import os
-import time
-
-from .color import color
-
-VERBOSE, NORMAL, WARN, ERROR = range(4)
-
-
-def log(message, level=NORMAL):
-    messages = []
-    if level > WARN:
-        text = 'WARNING'
-        if level == ERROR:
-            text = 'ERROR'
-        messages.append('[{}]'.format(color(text, 31)))
-
-    messages.append(message)
-    print(time.strftime('[%H:%M:%S]'), *messages)
 
 def setup_logging():
     logging.basicConfig(format='[%(asctime)s] %(levelname)s %(message)s', level=logging.DEBUG)
