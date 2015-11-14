fe19f198198fd34e7925f9b09f458b11f6b3803c
Louis Taylor <louis@kragniz.eu>
2015-11-14 23:06:18 +0000

    Add locking to driver handlers

--- boatd/driver.py
+++ boatd/driver.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 from functools import wraps
 
 from .color import color
@@ -23,12 +24,20 @@ class Driver(object):
         self.handlers = {}
 
     def handler(self, name, function=None):
+        '''
+        Handler function for a particular item in the driver. This implements
+        logging and locking for thread safet.
+        '''
         def wrapper(f):
+            lock = threading.Lock()
+
             @wraps(f)
             def dec(*args, **kwargs):
                 if callable(function):
                     function(f.__name__, args)
-                return f(*args, **kwargs)
+                with lock:
+                    return f(*args, **kwargs)
+
             self.handlers[name] = dec
             log.info('Loaded function {} as {}'.format(
                      color(f.__name__, 32),
