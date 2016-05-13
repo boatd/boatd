dece03e4f7ec4624a5af04054134acdcf412ef39
Louis Taylor <louis@kragniz.eu>
2016-05-13 01:18:22 +0100

    Properly implement logging plugin

--- boatd/coreplugins/logger.py
+++ boatd/coreplugins/logger.py
@@ -1,13 +1,28 @@
 from boatd import BasePlugin
 
+import datetime
 import time
 
 
 class LoggerPlugin(BasePlugin):
     def main(self):
+        period = self.config.period
+        filename = self.config.filename
+
         while self.running:
-            position = self.boatd.boat.position()
-            print('logging some crap -', position)
-            time.sleep(1)
+            lat, lon = self.boatd.boat.position()
+
+            ts = time.time()
+
+            log_line = '{} lat:{} long:{}\n'.format(
+                    datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'),
+                    lat,
+                    lon
+            )
+
+            with open(filename, 'a') as f:
+                f.write(log_line)
+
+            time.sleep(period)
 
 plugin = LoggerPlugin
