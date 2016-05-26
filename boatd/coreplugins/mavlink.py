8ff7e99942a21827499addcf58c8460cfb8b4987
Louis Taylor <louis@kragniz.eu>
2016-05-26 03:23:58 +0100

    Add basic heading mavlink communication

--- boatd/coreplugins/mavlink.py
+++ boatd/coreplugins/mavlink.py
@@ -24,7 +24,7 @@ class MavlinkPlugin(BasePlugin):
             int(-4.09098 * 1e7),
             25000,
             25000,
-            1,1,1,180)
+            1,1,1,180*100)
 
         self.ml.gps_raw_int_send(
             int(time.time()*100),
@@ -39,6 +39,31 @@ class MavlinkPlugin(BasePlugin):
             5
         )
 
+    def send_heading(self, heading):
+        heading = (heading % 360) - 180
+        self.ml.vfr_hud_send(
+            0,  # airspeed
+            10,  # groundspeed
+            heading,  # heading
+            100,  # throttle
+            0,  # alt
+            0  # climb
+        )
+
+    def send_param(self, name, value, index):
+        self.ml.param_value_send(
+            name,
+            value,
+            mv.MAV_PARAM_TYPE_REAL32,
+            len(self.params),
+            index
+        )
+
+    def send_params(self):
+        for param in self.params:
+            name, value = param
+            self.send_param(name, value, 0)
+
     def main(self):
         #period = self.config.period
         #filename = self.config.filename
@@ -47,9 +72,16 @@ class MavlinkPlugin(BasePlugin):
         self.ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.1)
         self.ml = mv.MAVLink(self.ser)
 
+        self.params = [(b'RUDDER', 0.5)]
+
+        i = 0
+
         while self.running:
             self.send_heartbeat()
             self.send_position()
+            i += 1
+            print('heading:', i)
+            self.send_heading(i)
 
             lat, lon = self.boatd.boat.position()
 
@@ -59,6 +91,8 @@ class MavlinkPlugin(BasePlugin):
                 for message in messages:
                     name = message.get_type()
                     print(name)
+                    if name == 'PARAM_REQUEST_LIST':
+                        self.send_params()
 
             time.sleep(0.01) 
 
