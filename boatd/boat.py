4a003465ae75515c1fb0195056c3391447cc88a4
Louis Taylor <louis@kragniz.eu>
2016-06-07 22:52:28 +0100

    Cache sensor values

--- boatd/boat.py
+++ boatd/boat.py
@@ -1,4 +1,6 @@
 import logging
+import threading
+import time
 
 log = logging.getLogger(__name__)
 
@@ -9,21 +11,44 @@ class Boat(object):
         self.driver = driver
         self.active = False
 
+        self._cached_heading = 0
+        self._cached_wind_speed = 0
+        self._cached_wind_direction = 0
+        self._cached_position = (0, 0)
+        self._cached_rudder_position = 0
+        self._cached_sail_position = 0
+
+        self._update_thread = threading.Thread(target=self.update_cached_values)
+        self._update_thread.start()
+
+    def update_cached_values(self):
+        '''Run in background and periodically update sensor values.'''
+        while True:
+            try:
+                self._cached_heading = self.driver.heading()
+                self._cached_wind_speed = self.driver.wind_speed()
+                self._cached_wind_direction = self.driver.wind_direction()
+                self._cached_position = self.driver.position()
+            except Exception as e:
+                log.error('Got error when trying to update sensor values: '
+                          '{}'.format(e))
+            time.sleep(0.2)
+
     def __getattr__(self, name):
         '''Return the requested attribute from the currently loaded driver'''
         return self.driver.handlers.get(name)
 
     def heading(self):
-        return self.driver.heading()
+        return self._cached_heading
 
     def wind_speed(self):
-        return self.driver.wind_speed()
+        return self._cached_wind_speed
 
     def wind_direction(self):
-        return self.driver.wind_direction()
+        return self._cached_wind_direction
 
     def position(self):
-        return self.driver.position()
+        return self._cached_position
 
     def rudder(self, angle):
         log.debug('setting rudder angle to {}'.format(angle))
