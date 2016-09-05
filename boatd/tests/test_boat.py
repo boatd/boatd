045573addcd202e1e54973a695d6b2f1c863d58d
Louis Taylor <louis@kragniz.eu>
2016-09-05 10:22:57 +0100

    Change mock boat to use integer

--- boatd/tests/test_boat.py
+++ boatd/tests/test_boat.py
@@ -4,14 +4,14 @@ import boatd
 
 class MockDriver(object):
     def heading(self):
-        return 2.43
+        return 2
 
 class TestBoat(unittest.TestCase):
     def setUp(self):
         self.boat = boatd.Boat(MockDriver())
 
     def test_get_heading(self):
-        assert self.boat.heading() == 2.43
+        assert self.boat.heading() == 2
 
     def test_active(self):
         assert not self.boat.active
