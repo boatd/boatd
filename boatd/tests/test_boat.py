687d3025bcfebee28e14aa6b0f4209066f8fd4eb
Louis Taylor <louis@kragniz.eu>
2016-09-05 10:36:44 +0100

    Update cached sensor values in boat test

--- boatd/tests/test_boat.py
+++ boatd/tests/test_boat.py
@@ -4,14 +4,15 @@ import boatd
 
 class MockDriver(object):
     def heading(self):
-        return 2
+        return 2.43
 
 class TestBoat(unittest.TestCase):
     def setUp(self):
         self.boat = boatd.Boat(MockDriver())
+        self.boat.update_cached_values()
 
     def test_get_heading(self):
-        assert self.boat.heading() == 2
+        assert self.boat.heading() == 2.43
 
     def test_active(self):
         assert not self.boat.active
